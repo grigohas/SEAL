b17dc73d61e66c0e0c852ebcc9a713eb65a9034d
grigohas <grigohas@gmail.com>
2025-02-21 14:33:45 +0200

    Update EnableCXXCompilerFlags.cmake

--- cmake/EnableCXXCompilerFlags.cmake
+++ cmake/EnableCXXCompilerFlags.cmake
@@ -25,5 +25,8 @@ if(NOT MSVC AND SEAL_DEBUG)
     seal_enable_cxx_compiler_flag_if_supported("-O3")
     seal_enable_cxx_compiler_flag_if_supported("-Werror")
     seal_enable_cxx_compiler_flag_if_supported("-g")
+    seal_enable_cxx_compiler_flag_if_supported("-march=rv64gcv")
+    seal_enable_cxx_compiler_flag_if_supported("-mabi=lp64d")
+    
 endif()
 
