20742ec0b1e4ca083596714ad76d1ba6b6376229
grigohas <grigohas@gmail.com>
2025-04-22 12:48:19 +0300

    Update examples.cpp

--- native/examples/examples.cpp
+++ native/examples/examples.cpp
@@ -9,7 +9,9 @@ using namespace seal;
 int main()
 {
     cout << "Microsoft SEAL version: " << SEAL_VERSION << endl;
-    while (true)
+    example_bfv_basics();
+    
+    /*while (true)
     {
         cout << "+---------------------------------------------------------+" << endl;
         cout << "| The following examples should be executed while reading |" << endl;
@@ -27,12 +29,12 @@ int main()
         cout << "| 8. Performance Test        | 8_performance.cpp          |" << endl;
         cout << "+----------------------------+----------------------------+" << endl;
 
-        /*
+        
         Print how much memory we have allocated from the current memory pool.
         By default the memory pool will be a static global pool and the
         MemoryManager class can be used to change it. Most users should have
         little or no reason to touch the memory allocation system.
-        */
+        
         size_t megabytes = MemoryManager::GetPool().alloc_byte_count() >> 20;
         cout << "[" << setw(7) << right << megabytes << " MB] "
              << "Total allocation from the memory pool" << endl;
@@ -99,7 +101,7 @@ int main()
         case 0:
             return 0;
         }
-    }
+    } */
 
     return 0;
 }
