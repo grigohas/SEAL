72fc5fea5e16c605cd64187ad411cda04b641bf8
grigohas <grigohas@gmail.com>
2025-05-15 12:35:24 +0300

    Update ntt.h

--- native/src/seal/util/ntt.h
+++ native/src/seal/util/ntt.h
@@ -13,6 +13,11 @@
 #include "seal/util/uintcore.h"
 #include <stdexcept>
 
+#ifdef __riscv_vector
+#include <riscv_vector.h>
+#endif
+
+
 namespace seal
 {
     namespace util
@@ -59,6 +64,104 @@ namespace seal
             {
                 return SEAL_COND_SELECT(a >= two_times_modulus_, a - two_times_modulus_, a);
             }
+
+            #if defined(__riscv_v_intrinsic)
+              inline void guard_vector_m4(const uint64_t *in,  uint64_t *out, size_t n) const {
+                  size_t vl;
+                  size_t i=0;
+                  
+                  while(i<n) {
+                      vl = __riscv_vsetvl_e64m4(n - i);
+                      
+                      // Load input data
+                      vuint64m4_t vec = __riscv_vle64_v_u64m4(&in[i], vl);
+                      
+                      // Create a vector with all elements set to two_times_modulus
+                      vuint64m4_t modulus_vec = __riscv_vmv_v_x_u64m4(two_times_modulus_, vl);
+                      
+                      // Create a mask for elements >= two_times_modulus
+                      vbool16_t mask = __riscv_vmsgeu_vx_u64m4_b16(vec, two_times_modulus_, vl);
+                      
+                      // Subtract two_times_modulus where mask is true
+                      vuint64m4_t sub_result = __riscv_vsub_vv_u64m4_mu(mask, vec, vec, modulus_vec, vl);
+                      
+                      // Store results
+                      __riscv_vse64_v_u64m4(&out[i], sub_result, vl);
+                      
+                      
+                      i += vl;
+                  }
+              }
+              
+              inline void add_vector_u64(const uint64_t* a, const uint64_t* b,  uint64_t* result, size_t n) const{
+                  size_t vl;
+                  size_t i = 0;
+                  while (i < n) {
+                      vl = __riscv_vsetvl_e64m4(n - i);                        // Set vector length for u64m4 (4 elements per vector)
+                      vuint64m4_t va = __riscv_vle64_v_u64m4(&a[i], vl);       // Load a[i..i+vl]
+                      vuint64m4_t vb = __riscv_vle64_v_u64m4(&b[i], vl);       // Load b[i..i+vl]
+                      vuint64m4_t vsum = __riscv_vadd_vv_u64m4(va, vb, vl);    // Add vectors
+                      __riscv_vse64_v_u64m4(&result[i], vsum, vl);             // Store result
+                      i += vl;
+                  }
+              }
+              
+              inline void sub_vector_u64_mod(const uint64_t* a, const uint64_t* b,  uint64_t* result, size_t n) const {
+                  size_t vl;
+                  size_t i = 0;
+                  while (i < n) {
+                      vl = __riscv_vsetvl_e64m4(n - i);  // Set vector length for u64m4 (4 elements per vector)
+              
+                      vuint64m4_t va = __riscv_vle64_v_u64m4(&a[i], vl);
+                      vuint64m4_t vb = __riscv_vle64_v_u64m4(&b[i], vl);
+                      vuint64m4_t vmod2 = __riscv_vmv_v_x_u64m4(two_times_modulus_, vl);  // Replicate modulus
+              
+                      // result = a + (2 * modulus) - b
+                      vuint64m4_t vtmp = __riscv_vadd_vv_u64m4(va, vmod2, vl);
+                      vuint64m4_t vres = __riscv_vsub_vv_u64m4(vtmp, vb, vl);
+              
+                      __riscv_vse64_v_u64m4(&result[i], vres, vl);
+                      i += vl;
+                  }
+              }
+              
+              inline void mul_vector_u64_mod(const uint64_t* a, const uint64_t yquot, const uint64_t yop,  uint64_t* result, size_t n) const {
+                  size_t vl;
+                  size_t i = 0;
+                  const uint64_t p = modulus_.value();  // Assuming this is in scope
+              
+                  while (i < n) {
+                      // Set vector length
+                      vl = __riscv_vsetvl_e64m4(n - i);  // Using m4 for vector length
+              
+                      // Load the input vectors into 64-bit vector registers
+                      vuint64m4_t va = __riscv_vle64_v_u64m4(&a[i], vl);
+                      vuint64m4_t vb = __riscv_vmv_v_x_u64m4(yquot, vl);  // yquot replicated across vector
+                      vuint64m4_t vp = __riscv_vmv_v_x_u64m4(p, vl);      // p replicated across vector
+                      vuint64m4_t vop = __riscv_vmv_v_x_u64m4(yop, vl);    // yop replicated across vector
+              
+                      // Perform high part of multiplication (multiplication with unsigned high result)
+                      vuint64m4_t vhi = __riscv_vmulhu_vv_u64m4(va, vb, vl);  // unsigned high part of va * vb
+              
+                      // Perform multiplication with yop
+                      vuint64m4_t vmul1 = __riscv_vmul_vv_u64m4(va, vop, vl);  // va * yop
+              
+                      // Perform multiplication with p for modulus operation
+                      vuint64m4_t vmul2 = __riscv_vmul_vv_u64m4(vhi, vp, vl);  // vhi * p
+              
+                      // Subtract vmul2 from vmul1 to get the result (before modulo reduction)
+                      vuint64m4_t vres = __riscv_vsub_vv_u64m4(vmul1, vmul2, vl);  // vmul1 - vmul2
+              
+                      // Store the result in the output array
+                      __riscv_vse64_v_u64m4(&result[i], vres, vl);
+              
+                      // Increment the index by the vector length (processed elements)
+                      i += vl;
+                  }
+                }
+              #endif
+
+
 
         private:
             Modulus modulus_;
