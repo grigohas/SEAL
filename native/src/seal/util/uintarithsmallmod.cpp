efc441d26841ac8ac7d12dfad69ac7036cf30c7e
grigohas <grigohas@gmail.com>
2025-05-22 14:23:44 +0300

    Update uintarithsmallmod.cpp

--- native/src/seal/util/uintarithsmallmod.cpp
+++ native/src/seal/util/uintarithsmallmod.cpp
@@ -13,6 +13,9 @@
 #endif
 
 using namespace std;
+using namespace std::chrono;
+
+extern int f;
 
 namespace seal
 {
@@ -156,6 +159,7 @@ namespace seal
         {
             static_assert(SEAL_MULTIPLY_ACCUMULATE_MOD_MAX >= 16, "SEAL_MULTIPLY_ACCUMULATE_MOD_MAX");
             unsigned long long accumulator[2]{ 0, 0 };
+            auto start3 = high_resolution_clock::now();
             #if defined(__riscv_v_intrinsic) 
               vector_mult_accumulate_u64_to_u128(operand1,operand2,count,(long long*)accumulator);
             #else
@@ -217,6 +221,9 @@ namespace seal
                 goto largest_case;
             };
             #endif
+            auto stop3 = high_resolution_clock::now();
+   	          auto duration3 = duration_cast<microseconds>(stop3 - start3);
+              f+=duration3.count();
             return barrett_reduce_128(accumulator, modulus);
         }
     } // namespace util
