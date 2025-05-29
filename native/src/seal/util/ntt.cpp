fe3595ef2a816d09c02341fd85c2d66caac353e9
grigohas <grigohas@gmail.com>
2025-05-29 20:21:12 +0300

    Update ntt.cpp

--- native/src/seal/util/ntt.cpp
+++ native/src/seal/util/ntt.cpp
@@ -12,6 +12,7 @@
 #include "seal/util/locks.h"
 #include "seal/util/pointer.h"
 #include <unordered_map>
+#include <omp.h>
 #include "hexl/hexl.hpp"
 #endif
 #ifdef __riscv_vector
@@ -248,7 +249,7 @@ namespace seal
             vuint64m4_t v_rem = __riscv_vmv_v_x_u64m4(0, vl);
             
             // Process upper 64 bits from num_hi
-            
+           //#pragma GCC unroll 4
            for (int j = 0; j < 64; j++) {
                 v_rem = __riscv_vsll_vx_u64m4(v_rem, 1, vl);
                 vuint64m4_t next_bit = __riscv_vsrl_vx_u64m4(num_hi, 63, vl);
@@ -262,7 +263,7 @@ namespace seal
            }
             
             // Process lower 64 bits from num_lo
-           
+           // #pragma GCC unroll 4
             for (int j = 0; j < 64; j++) {
                 v_rem = __riscv_vsll_vx_u64m4(v_rem, 1, vl);
                 vuint64m4_t next_bit = __riscv_vsrl_vx_u64m4(num_lo, 63, vl);
@@ -335,8 +336,10 @@ namespace seal
                 for (size_t i = 1; i < coeff_count_; i++) {
                     num[i] = multiply_uint_mod(num[i-1], root, modulus_);
                 }
-                size_t processed=0;
                 
+                
+                size_t processed=0;
+               
                 
                 size_t vl = __riscv_vsetvl_e64m4(coeff_count_-1 - processed);
                 vuint64m4_t den_vec = __riscv_vmv_v_x_u64m4(denom, vl);
@@ -351,7 +354,7 @@ namespace seal
                 }
                 
                 
-                
+                #pragma omp parallel for
                 for(size_t i = 1; i < coeff_count_; i++){
                     size_t rev = reverse_bits(i, coeff_count_power_);
                     root_powers_[rev].operand = num[i - 1];
@@ -387,7 +390,7 @@ namespace seal
                 num1[i] = multiply_uint_mod(num1[i-1], root, modulus_);
             }
             
-        
+            
             processed=0;
             
             
@@ -403,7 +406,7 @@ namespace seal
             }
             
             
-            
+            #pragma omp parallel for
             for(size_t i = 1; i < coeff_count_; i++){
                 size_t rev = reverse_bits(i-1, coeff_count_power_)+1;
                 inv_root_powers_[rev].operand = num1[i - 1];
